df27d9ac415a10cd32eb940d914df5987b3bc35e
Rafiq Shaik <rafiqshaik097@gmail.com>
2025-08-09 12:07:15 +0530

    configure environment variables for database connection and add .env to .gitignore

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, ValidationError
-import bcrypt
+from flask import Flask, request, jsonify, session
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+from flask_cors import CORS
+import bcrypt
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
-app.config['MYSQL_HOST'] = ''
-app.config['MYSQL_USER'] = ''
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = ''
-app.secret_key = ''
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.secret_key = os.getenv('SECRET_KEY')
 
 mysql = MySQL(app)
 
