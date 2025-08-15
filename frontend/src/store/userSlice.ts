e184a716273f1d28dcefbcc0a561016e99b25b7b
Rafiq Shaik <rafiqshaik097@gmail.com>
2025-08-15 11:54:22 +0530

    chamges

--- frontend/src/store/userSlice.ts
+++ frontend/src/store/userSlice.ts
@@ -1,18 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-    name: 'user',
-    initialState: null,
-    reducers: {
-        setUser: (state: any, action: any) => {
-            state = action.payload
-        },
-        clearUser: (state: any) => {
-            state = null;
-        }
-    }
+  name: "user",
+  initialState: null,
+  reducers: {
+    setUser: (state: any, action: any) => {
+      state = action.payload;
+    },
+    clearUser: (state: any) => {
+      state = null;
+    },
+  },
 });
 
 export const { setUser, clearUser } = userSlice.actions;
